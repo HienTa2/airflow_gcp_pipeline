dbc338e785aa6a35005332d70b7d48a3f6889b65
HienTa2 <109198960+HienTa2@users.noreply.github.com>
2024-12-13 11:27:21 -0600

    Update upload_to_gcs.py

--- cloud_functions/upload_to_gcs.py
+++ cloud_functions/upload_to_gcs.py
@@ -1,1 +1,15 @@
+from google.cloud import storage
+
+def list_files(bucket_name):
+    """Lists all files in the bucket."""
+    storage_client = storage.Client()
+    blobs = storage_client.list_blobs(bucket_name)
+
+    print(f"Files in {bucket_name}:")
+    for blob in blobs:
+        print(blob.name)
+
+if __name__ == "__main__":
+    bucket_name = "your_bucket_name"
+    list_files(bucket_name)
 
